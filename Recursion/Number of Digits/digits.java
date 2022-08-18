3e79bec4bb9f09f4a10521481a543534e40beb34
ranjan1310 <59019447+ranjan1310@users.noreply.github.com>
2022-08-18 23:38:39 +0530

    feat
    Submitting new program using recursion

---
+++ Recursion/Number of Digits/digits.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+public class digits{
+    public static int count(int n){
+        if(n==0){
+            return 0;
+        }
+        int ans =count(n/10);
+        return ans+1;
+
+    }
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        
+            int n = s.nextInt();
+            System.out.println(count(n));
+        
+            s.close();
+        
+    }
+
+}
